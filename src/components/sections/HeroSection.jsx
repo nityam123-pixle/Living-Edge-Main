ba5777bc6ce3de3268b8dcee1e213a393044e7de
Nityam Suchak <83059130+nityam123-pixle@users.noreply.github.com>
2025-06-09 13:32:51 +0530

    Update HeroSection.jsx

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
         </motion.a>
 
         <motion.a
-          href="#"
+          href="#contact"
           initial={{ opacity: 0, y: -20 }}
           animate={animateRest ? { opacity: 1, y: 0 } : {}}
           transition={{ delay: 1.2, duration: 0.5 }}
